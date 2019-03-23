7f25adaf763b051aba3d6ed371dd148eba00b326
PlasticHead21 <PlasticHead23@gmail.com>
2019-03-23 18:45:46 +0300

    Create New() action in MoviesController and add Details and MoviesForm views.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using System.Data.Entity;
+using Vidly.ViewsModels;
 
 namespace Vidly.Controllers
 {
@@ -20,6 +21,24 @@ namespace Vidly.Controllers
         {
             var movies = _context.Movie.ToList();
             return View(movies);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            var movie = _context.Movie.Include(m => m.Genre).SingleOrDefault(m => m.ID == id);
+            if (movie is null) return HttpNotFound();
+            return View(movie);
+        }
+
+        public ActionResult New()
+        {
+            var genres = _context.Genres.ToList();
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres,
+
+            };
+            return View();
         }
     }
 }
