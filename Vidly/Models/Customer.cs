eb34f1fd5c7f76e44b901342c4aea3669e05800b
Denis M <plastichead23@gmail.com>
2019-03-26 01:11:20 +0300

    Start to creating WebApi. Namely created CustomersController with all CRUD operations.

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -8,8 +8,8 @@ namespace Vidly.Models
 {
     public class Customer
     {
-        [Required]
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
